58713b90c93335a28836b3c16d79ed2198512c08
Rodrigo Hofer <digoalbano1010@gmail.com>
2022-07-23 14:09:54 -0300

    update
    update no codigo js da mascara de telefone

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-function formatar(mascara, documento){
-    var i = documento.value.length;
-    var saida = mascara.substring(0,1);
-    var texto = mascara.substring(i)
-    
-    if (texto.substring(0,1) != saida){
-        documento.value += texto.substring(0,1);
-    }
-    
-}
+$(document).ready(function(){
+	$("#phone").mask("(99) 99999-9999");
+});
